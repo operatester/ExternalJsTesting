27faaf4c55660934c44ebb605f1922700690c457
dmichnowicz@opera.com <dmichnowicz@opera.com>
2016-11-25 07:05:22 +0100

    new files

---
+++ s2.js
@@ -0,0 +1,5 @@
+(function(){
+    var s = document.createElement('script');
+    s.src = '//raw.githubusercontent.com/operatester/ExternalJsTesting/master/a1.js';
+    document.body.appendChild(s);
+})();
