06006590907e25ffa94e44489683a500a235169f
dmichnowicz@opera.com <dmichnowicz@opera.com>
2016-11-25 07:24:58 +0100

    fixes

--- s2.js
+++ s2.js
@@ -1,5 +1,5 @@
 (function(){
     var s = document.createElement('script');
-    s.src = '//raw.githubusercontent.com/operatester/ExternalJsTesting/master/a1.js';
+    s.src = '//cdn.rawgit.com/operatester/ExternalJsTesting/master/a1.js';
     document.body.appendChild(s);
 })();
